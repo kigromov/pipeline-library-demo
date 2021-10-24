e9fc278172cebfbaaac2fed558ec78a2ac48a66d
kgromov <kigromov@users.noreply.github.com>
2021-10-24 16:28:30 +0300

    add new stage

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -5,5 +5,14 @@ def call(String name = 'human') {
     {
         build job: 'AllureTest/allure', parameters: [string(name: 'testList', value: '*')]
     }
+               stage("get failed test") {
+                def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+                def failedTests = testResultAction.getFailedTests()
+                def failedTestsString = "```"
+                for(CaseResult cr : failedTests) {
+                    failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
+                }
+                echo failedTestsString
+            }
 }
 
