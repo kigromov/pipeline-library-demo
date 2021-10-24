e51622e4b11a4ea1c2189548fb1c5662b4a58fb7
kgromov <kigromov@users.noreply.github.com>
2021-10-24 16:32:23 +0300

    res

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -5,15 +5,14 @@ def call(String name = 'human') {
    stage("start test job") 
     {
         build job: 'AllureTest/allure', parameters: [string(name: 'testList', value: '*')]
+        def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+        def failedTests = testResultAction.getFailedTests()
+        def failedTestsString = "```"
+        for(CaseResult cr : failedTests) {
+            failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
+         }
+        echo failedTestsString
     }
-               stage("get failed test") {
-                def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-                def failedTests = testResultAction.getFailedTests()
-                def failedTestsString = "```"
-                for(CaseResult cr : failedTests) {
-                    failedTestsString = failedTestsString + "${cr.getFullDisplayName()}:\n${cr.getErrorDetails()}\n\n"
-                }
-                echo failedTestsString
-            }
+            
 }
 
