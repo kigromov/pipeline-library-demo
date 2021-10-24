8c820b13e1b5fc38f1432f9ef8328717fb283b98
kgromov <kigromov@users.noreply.github.com>
2021-10-24 20:26:55 +0300

    ddd

--- vars/runTestJob.groovy
+++ vars/runTestJob.groovy
@@ -3,12 +3,6 @@ def call(Map<String, String> params) {
    echo "runTestJob started" 
    def testList = params["testList"]
    echo testList
-   def cBuild
-   stage("start test job") 
-    {
-       cBuild = build job: 'AllureTest/allure', propagate: false, wait: true, parameters: [string(name: 'testList', value: testList)]
-      
-    }
-    return cBuild
-            
+   def cBuild = build job: 'AllureTest/allure', propagate: false, wait: true, parameters: [string(name: 'testList', value: testList)]
+   return cBuild         
 }
