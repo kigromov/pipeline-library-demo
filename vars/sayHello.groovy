83619a4ef0b9a84a59606d3d7abcdd578433b299
kgromov <kigromov@users.noreply.github.com>
2021-10-24 21:43:19 +0300

    df

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -2,27 +2,22 @@
 import hudson.tasks.test.AbstractTestResultAction
 import hudson.tasks.junit.CaseResult
 
-def call(Map<String, String> params) {
+def call(Map<String, String> params) 
+{
    def testList = params["testList"]
-
-       echo testList
-       echo "START 1 JOB"
    def currentBuild
-  for (int i = 0; i < 2; i++) {
+   for (int i = 0; i < 2; i++) 
+   {
       stage("start test job ${i}") 
-    {
-       currentBuild = runTestJob(testList: testList)
-    
-       echo "END 1 JOB"
-       
-       def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
-       def failedTests = testResultAction.getFailedTests()
-       for(CaseResult cr : failedTests) {
-           testList = testList + "${cr.getFullDisplayName()},"
+        {
+           currentBuild = runTestJob(testList: testList)
+           def testResultAction = currentBuild.rawBuild.getAction(AbstractTestResultAction.class)
+           def failedTests = testResultAction.getFailedTests()
+           for(CaseResult cr : failedTests) {
+               testList = testList + "FullyQualifiedName~${cr.getFullDisplayName()} | "
+           }
+         echo testList
         }
-       echo testList
-       
-    }
-  }
+   }
 }
 
