0a9a94b9a02bb333c400489a9f2881321fccd308
kgromov <kigromov@users.noreply.github.com>
2021-10-24 18:34:38 +0300

    runTestJob

--- vars/runTestJob.groovy
+++ vars/runTestJob.groovy
@@ -1,1 +1,11 @@
-
+#!/usr/bin/env groovy
+def call(Map<String, String> params) {
+   def testList = params["testList"] ?: "*Test"
+   echo testList
+   stage("start test job") 
+    {
+       def cBuild = build job: 'AllureTest/allure', propagate: false, wait: true, parameters: [string(name: 'testList', value: '*')]
+       return cBuild
+    }
+            
+}
